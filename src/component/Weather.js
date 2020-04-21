7ae81dda8513ab4587262bf489146890f32721ed
Your Name <you@example.com>
2020-04-21 13:44:20 +0530

    add the check weather

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import axios from  'axios'
+// import {withStyles} from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 class Weather extends Component {
     constructor(props){
         super(props);
@@ -9,15 +11,19 @@ class Weather extends Component {
     }
 
 async componentDidMount (){
-    const response = await axios.get('http://api.weatherstack.com/current?access_key=ad1c16a10230bed3168076c957f51552query=' + this.props.location.state.image,{
+    const response = await axios.get('http://api.weatherstack.com/current?access_key=1a93a72ba85c81df32e8ba53280900e1&query=' + this.props.location.state.image,{
 
      })
      console.log(response)
+     this.setState({data:response.data.current})
 }
     render() {
         return (
             <div>
-                
+                <img alt='thumbnail' src={this.state.data.weather_icons}/>
+               <Typography>{this.state.data.temperature}</Typography> 
+               <Typography>{this.state.data.wind_speed}</Typography> 
+               <Typography>{this.state.data.precip}</Typography> 
             </div>
         )
     }
